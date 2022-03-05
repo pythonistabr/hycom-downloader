d785f117eb0610aba83cdc6a60cc7b05dc8b3268
pythonistabr <80839383+pythonistabr@users.noreply.github.com>
2022-03-05 12:59:43 -0300

    Update dates_generator.py

--- dates_generator.py
+++ dates_generator.py
@@ -3,71 +3,41 @@ Author: @pythonistabr
 contact: gustavo.oceanografia@gmail.com
 
 """
-from copy import copy
-from datetime import datetime 
-from datetime import timedelta
+import numpy as np
 
 class DatesGenerator:
-    
-    def __init__(self):
-        self._items = []
 
-    def generateDates(self, date_min, date_max, time_step):
-        self.date_min = date_min
-        self.date_max = date_max
-        self.time_step = time_step
-        self.date1 = datetime.strptime(self.date_min,'%Y-%m-%d')
-        self.date2 = datetime.strptime(self.date_max,'%Y-%m-%d')
-
-        self.range_of_dates = self.date2 - self.date1
-        self.range_of_dates = str(self.range_of_dates)
-        self.range_of_dates = self.range_of_dates[0: self.range_of_dates.find(' ')+1]
-        self.range_of_dates = int(self.range_of_dates) // self.time_step
-
-        date1 = copy(self.date1)
-
-        for ii in range(0, self.range_of_dates):
-
-            date2 = date1 + timedelta(days = self.time_step)
-
-            date1_formated = '{}-{:02d}-{:02d}'.format(date1.year,
-            date1.month, date1.day )
-            
-            date2_formated = '{}-{:02d}-{:02d}'.format(date2.year,
-            date2.month, date2.day )
+    def generateDates(self, date_min, date_max):
         
-            self._items.append((date1_formated, date2_formated))
-            
-            date1 = date2
+        self._dates = np.arange(np.datetime64(date_min),
+        np.datetime64(date_max),
+        dtype='datetime64[D]')
 
-        return self._items
+        return self._dates
 
-    def __iter__(self):
-        return iter(self._items)
+    @property
+    def start_dates(self):
+        return self._dates[0:len(self._dates):2]
 
-    def __len__(self):
-        return len(self._items)
-    
-
-    def __getitem__(self, index):
-        return self._items[index]
- 
-    
-    def __str__(self):
-        length = len(self._items)
-        return "Date List has {} items".format(length)
-    
-    
-    
+    @property
+    def end_dates(self):
+        return self._dates[1:len(self._dates):2]
+  
 
 def main():
+    
     myobject = DatesGenerator()
-    myobject.generateDates('2017-10-01','2017-12-31', 7)
+    myobject.generateDates('2016-12-02','2017-01-31')
+
+    print(myobject.start_dates[0])
+    print()
+    print(myobject.end_dates[0])
+
     
-    for ii in myobject:
-        print(ii)
-    
-    print(myobject)
+    #print(myobject) 
+if __name__ == "__main__":
+    main() DatesGenerator:
+
     
 if __name__ == "__main__":
     main()
